fd74fd005560479ead51f17994123d30a2f478c3
lsner <740501011@qq.com>
2018-11-29 15:38:17 +0800

    add 注释

--- src/fetch.js
+++ src/fetch.js
@@ -7,3 +7,9 @@ const normalFetch = function (promiseRequest,limitTime) {
 
     return abortPromiese
 }
+
+
+/* 
+Promise.race 返回的是最先有结果的那个promise 无论结果是resolve 还是 reject
+Prpmise.all 依次执行，如果都成功则返回一个resolve的list 但只要有一个reject 后面的都不会执行 并且会返回reject的error message
+*/
