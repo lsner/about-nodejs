92ba2af0b921f0f8f28c5b80fe5f711d3d579cd2
lsner <740501011@qq.com>
2019-04-19 11:19:48 +0800

    clear

--- some emmmmm/str_reverse.js
+++
@@ -1,9 +0,0 @@
-function getReserve(num) {
-    let numArray = num.toString().split("");
-    numArray.reverse();
-    let newNum = numArray.join("");
-    return Number(num) + Number(newNum)
-}
-
-const a = getReserve(100);
-console.log(a);
